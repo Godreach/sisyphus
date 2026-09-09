0581ab5cb9d9e841036366d455f79972cc4760e5
tanweijian <tanweijian199412@gmail.com>
2026-09-10 00:12:46 +0800

    style(web): 调整品牌标识资源与侧栏展示

--- sisyphus-web/src/App.vue
+++ sisyphus-web/src/App.vue
@@ -348,8 +348,7 @@ function onCta(): void {
             :style="{ flex: `0 0 ${sidebarWidth}px`, width: `${sidebarWidth}px` }"
           >
             <div class="sidebar-logo">
-              <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><rect fill="#2997FF" x="1" y="1" width="7" height="7" rx="2"/><rect fill="#2997FF" x="12" y="1" width="7" height="7" rx="2"/><rect fill="#2997FF" x="1" y="12" width="7" height="7" rx="2"/><rect fill="#5E5CE6" x="12" y="12" width="7" height="7" rx="2"/></svg>
-              <span>{{ t('app.name') }}</span>
+              <img class="sidebar-logo-image" src="/sisyphus-logo.svg" :alt="t('app.name')" width="1360" height="180" />
             </div>
             <nav class="sidebar-nav">
               <button
